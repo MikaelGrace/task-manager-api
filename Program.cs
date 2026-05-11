af21f5dfc6c5587882a3818818b219cbe2879d92
MikaelGrace <mikeokoduwa700@gmail.com>
2026-05-11 07:43:39 +0100

    Create Database Migraation: Add resource DLLs for multiple languages and update project files
    - Added Japanese, Korean, Polish, Portuguese (Brazil), Russian, Turkish, Simplified Chinese, and Traditional Chinese resource DLLs for Microsoft.CodeAnalysis and related assemblies.
    - Included new runtime libraries for Microsoft.Data.SqlClient and System.Drawing.Common for various platforms.
    - Updated TaskManagerAPI assembly information and cache files.
    - Added Entity Framework Core targets file for project metadata.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManagerAPI.Context;
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
