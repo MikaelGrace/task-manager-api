eab812293aca0e26d39c825b0acdf6d84a4bf0cb
MikaelGrace <mikeokoduwa700@gmail.com>
2026-05-11 08:20:59 +0100

    Add API controller setup and request examples for TaskManagerAPI Testing API endpoints Modified program.cs to map controller routes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManagerAPI.Context;
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers(); // add this
+
 var app = builder.Build();
+
+app.MapControllers(); // add this
 
 app.MapGet("/", () => "Hello World!");
 
